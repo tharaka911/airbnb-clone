103ee2a9dfa40df2fbfa273524d7116de3b2f34a
tharaka911 <tharakahitibandara@gmail.com>
2023-02-26 23:48:42 +0530

    props learned

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,20 @@ import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "./Data"
 
-
-console.log(data)
 export default function App() {
           
     const cards = data.map(item => {
         return (
             <Card 
-                img={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
+                key={item.id}
+                {...item}
+                //img={item.coverImg}
+                //rating={item.stats.rating}
+                //={item.stats.reviewCount}
+                //location={item.location}
+                //title={item.title}
+                //price={item.price}
+                //openSpots={item.openSpots}
             />
         )
     })        
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import imgstar from "../images/star.png";
 
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
   return (
     <div className="card">
-      <img src={props.img} className="card--img" />
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={props.coverImg} className="card--img" />
       <div className="card--stats">
         <img src={imgstar} className="card--star" />
-        <span>{props.rating}</span>
-        <span className="gray">({props.reviewCount}) • </span>
+        <span>{props.stats.rating}</span>
+        <span className="gray">({props.stats.reviewCount}) • </span>
         <span className="gray">{props.location}</span>
       </div>
       <p className="card--title">{props.title}</p>
